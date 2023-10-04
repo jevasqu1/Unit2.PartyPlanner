dafb66b6cfb0c5cce48eef43d2969f9f349f7c69
Jesse Vasquez <jr90box@gmail.com>
2023-10-04 07:24:45 -0700

    I wasnt able to figure out the code

---
+++ party.js
@@ -0,0 +1,8 @@
+//added the API as a variable to pull the information from the webpage into my JS
+const API_URL =
+  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309-FSA-ET-WEB-FT-SF/events";
+
+//declared my object state that I will be updated with the information from the API
+const state = {
+  parties: [],
+};
